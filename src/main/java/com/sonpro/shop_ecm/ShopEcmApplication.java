693c735d90b0cd4b10a9356a33dc179d4ad95bb7
Test Git <sonptph43120@gmail.com>
2024-08-29 16:20:14 +0700

    commit

---
+++ src/main/java/com/sonpro/shop_ecm/ShopEcmApplication.java
@@ -0,0 +1,13 @@
+package com.sonpro.shop_ecm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopEcmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopEcmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sonpro/shop_ecm/ShopEcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonpro.shop_ecm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopEcmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
